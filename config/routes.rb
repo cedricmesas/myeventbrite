2286b9993ac4e6cb7171c4dd500f3435266cce40
cedricmesas <mesas.cedric@gmail.com>
2021-05-18 15:37:13 +0200

    presque

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
-  get 'static_pages/event'
   devise_for :users
-  root to: "events#index"
+  resources :events
+  root "events#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
