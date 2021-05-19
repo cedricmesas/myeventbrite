28fa1cc4aed1e735d34d5641a263e0284b078b71
cedricmesas <mesas.cedric@gmail.com>
2021-05-19 08:54:46 +0200

    clear SEEDS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "date"
 require 'faker'
 
-User.destroy_all
-Event.destroy_all
-Attendance.destroy_all
+Participation.delete_all
+Event.delete_all
+User.delete_all
 
 10.times do
     User.create(
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        description: Faker::Lorem.sentences(2).join,
-        email:"@yopmail.com")
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.sentences(2).join,
+    email: Faker::Internet.safe_email,
+    password:Faker::Lorem.characters(number: 10)) 
 end
 
 10.times do
-    Event.create(
-        start_date: Faker::Date.between(from: '2021-05-18', to: '2025-09-25'),
-        duration: 150, 
-        title: Faker::String.random(length: 5..140).gsub("\u0000", ''), 
-        description: Faker::String.random(length: 20..1000).gsub("\u0000", ''),
-        price: Faker::Number.between(from: 1.0, to: 1000.0), 
-        location:Faker::Lorem.words(number: 1).join)
+    Event.create!(
+    start_date: DateTime.now, 
+    title: "YDNKJS", 
+    price: 1, 
+    description: "lorem ipsum lele", 
+    duration: 60, 
+    location: "no where", 
+    user_id: User.all.sample.id)
 end
 
-20.times do
-    Attendance.create(
-        user:User.all.sample, 
-        event: Event.all.sample, 
-        stripe_customer_id: Faker::String.random(length: 6..12))
+10.times do
+    Participation.create!(
+    event: Event.all.sample, 
+    user_id: User.all.sample.id, 
+    stripe_customer_id: Faker::Lorem.characters(number: 10))
 end
